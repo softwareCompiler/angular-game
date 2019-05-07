5c4aa42535e07ea5f2adecda5805f25ac5f6130d
Jun Zheng <jzheng3189@gmail.com>
2019-05-07 14:32:08 -0600

    initi canvas and draw

--- src/app/drag-game/drag-game.component.ts
+++ src/app/drag-game/drag-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, ViewChild, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-drag-game',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drag-game.component.css']
 })
 export class DragGameComponent implements OnInit {
-
+  @ViewChild('myCanvas') canvasRef: ElementRef;
   constructor() { }
 
   ngOnInit() {
+    const ctx = this.canvasRef.nativeElement.getContext('2d');
+    ctx.fillStyle = 'red';
+    ctx.fillRect(100, 75, 50, 50);
+    // ctx.fillStyle = 'red';
+    // ctx.fill();
+    ctx.beginPath();
+    ctx.arc(550, 100, 25, 0, 360);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
   }
-
 }
+
